5b15471272c1573fc2295d3d0c9b553ca171e9fc
jaydeninniss <jayden.inniss@gmail.com>
2024-07-06 18:55:26 -0700

    Done Desktop About + Footer
    To do:
    Make responsive
    Tada

---
+++ js/parallaxhero.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger)
+
+        gsap.to("#about-hero-bg", {
+            yPercent: 50,
+            ease: "none",
+            scrollTrigger: {
+                trigger: "#contact-hero",
+                start: "-30%", 
+
+                scrub: true
+            }, 
+        });
+
+        gsap.to("#about-hero-jayden", {
+            yPercent: 20,
+            ease: "none",
+            scrollTrigger: {
+                trigger: "#contact-hero",
+                start: "-30%", 
+
+                scrub: true
+            }, 
+        });
+
+        gsap.to("#about-hero-text", {
+            yPercent: 50,
+            ease: "none",
+            scrollTrigger: {
+                trigger: "#contact-hero",
+                start: "-30%", 
+
+                scrub: true
+            }, 
+        });
+
+    });
