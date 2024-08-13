060585f2939252c3a72a71f90194dc21143cb012
jaydeninniss <jayden.inniss@gmail.com>
2024-08-12 20:15:21 -0700

    Film Fest Page DONE!!! WOHOO

--- projects/stsfilmfest.php
+++ projects/stsfilmfest.php
@@ -42,9 +42,6 @@
         <!-- Header Img -->
         <div class="hero">
             <img src="../media/img/projects/stsfilmfestprojectcover.png" alt="">
-            <!-- <div class="project-title">
-                <h1>Sea to Sky Student Short Film Festival</h1>
-            </div> -->
         </div>
         <!-- END Header Img -->
 
@@ -59,30 +56,28 @@
         </div>
         <div class="content-container">
             <div class="qa-container">
-                <h3 class="question" id="first">The Goal</h3>
-                <p class="answer">This project aims to design an interface following UI/UX principles, and then improve the interface by identifying issues through user testing.</p>
-                <p class="answer"><strong>This project consisted of three phases:</strong></p>
-                <ul class="answer">
-                    <li>Phase 1: User research & designing the initial interface (Low-fidelity wireframes).</li>
-                    <li>Phase 2: Design high-fidelity interactive mockups. Then conduct User Testing to identify issues/weaknesses in the design.</li>
-                    <li>Phase 3: Edit the interface to address issues identified in user testing.</li>
-                </ul>
-                <p class="answer"><em>Phases 1 & 3 have accompanying reports.</em></p>
-            </div>
-
-            <div class="qa-container">
-                <h3 class="question">Phase 1</h3>
-                <p class="answer">Phase 1 began with establishing the topic of the website interface. I landed on an outdoor adventure planning tool as I use similar tools frequently planning my own adventures, so I understand what I like and what could be improved with existing websites.<br><br>Before design work could begin, the target audience and user goals had to be identified. To define the target user I created a detailed user persona, a user scenario, an empathy map and a journey map for an expected user.</p>
+                <h3 class="question" id="first">Overview</h3>
+                <p class="answer">My two peers and I created a film festival designed for youth in Whistler to showcase their filmmaking talent. Sho Brooks, Kenta Tanaka and I led this initiative. The competition was designed as a fundraiser for our graduating class at Whistler Secondary School. After receiving 9 submitted films, we held a viewing night at our school for an audience of over 300. This event was a huge success: We exceeded our projected profit by $4,000, doubled our expected audience and gave a platform for young aspiring filmmakers to display their work in front of the community. <br><br>Our judging panel was composed of 5 local professional photographers/videographers. The Whistler community generously donated over $2,000 worth of prizes including a brand-new GoPro, which was awarded to the filmmakers and the audience in raffle draws. <br><br>The event would not have been a success without the help of Shauna Hardy and John Hall.</p>
                 
-                <img src="../media/img/projects/uiux/deliverables/presentation/JaydenInniss_UIUXStrat1_FinalPresentation_Page_02.png" class="project-img" alt="">
-                
-                <div class="qa-container">
-                    <h3 class="question">Conclusion</h3>
-                    <p class="answer">In conclusion, I designed an effective interface that was tested to improve its design and functionality. Now I have a user-tested interface which is even more effective than before!</p>
-                </div>
-            </div>
-        </div>
+                <h3 class="question">Competition Details</h3>
+                <p class="answer">To celebrate what makes the Sea to Sky region unique, the Whistler Secondary Short Film Festival debuts as a student filmmaking competition produced by the graduating class of 2023 where you could win great prizes! The competition is open to anyone in grades 8 to 12 residing in Squamish, Whistler, Pemberton and Mt. Currie, and attending an SD48 public or independent school with a phone, GoPro, or camera with a story to tell. </p>
 
+                <h3 class="question">Advertising</h3>
+                <p class="answer">Creativity is your only limit. Student filmmakers are tasked to shoot, edit and produce a 3 to 5 minute short film within the Sea to Sky Corridor – taking their creative vision from concept to reality. The top ten films will premiere on the big screen for an audience of over 200 at Whistler Secondary. Over $2,000 in prizes will be awarded to the top three films plus a People’s Choice Award chosen by the audience. </p>
+
+                <p class="answer"><em>Calling Sea to Sky Corridor high school students! The Sea to Sky region is known for inspiring adventurers and storytellers. Whistler Secondary wants to see how it inspires you in its inaugural Short Film Festival. Submit a short film for your chance to win great prizes!</em></p>
+
+                <h3 class="question">Judging</h3>
+                <p class="answer">The judges, composed of professional filmmakers, will select up to 20 films, three to five minute films to be part of the Whistler Secondary Short Film Festival on April 20, 2023 at Whistler Secondary.<br><br>Entries will be judged based on their application’s overall concept, creativity, and originality.<br><br>There are no restrictions on your creativity or how you choose to capture the magic that makes the Sea to Sky region so unique. And there are no limitations on how the film is shot or how you interpret the theme. You can shoot live-action, animation, stop-motion, documentary, use actors, cutouts, puppets, or animals. The choice is yours!</p>
+
+                <h3 class="question">Film Screening Night</h3>
+                <p class="answer">After receiving 9 submitted films, we held a viewing night at our school for an audience of over 300. This event was a huge success: We exceeded our projected profit by $4,000, doubled our expected audience and gave a platform for young aspiring filmmakers to display their work in front of the community. <br><br>Myself and Kenta Tanaka hosted the night as MCs. </p>
+
+                <h3 class="question">Conclusion</h3>
+                <p class="answer">This event was a highlight of high school. Providing an opportunity for youth filmmakers in Whistler was a way to give back to the community for everything it has provided to me. In 2017, the World Ski and Snowboard Festival’s Olympus 72-Hour Filmmaker Showdown chose my film as one of the top 10 films. It was screened in front of 1,600, which was mind-blowing when I was in grade 7. This event was full circle as I was able to give filmmakers a similar opportunity to present their films and gain exposure within the community.<br><br>This whole event was so rewarding and I enjoyed every second of planning, developing and finally MCing the event alongside my incredibly talented peers: Sho Brooks and Kenta Tanaka.</p>
+                <p class="answer"><em>I will absolutely add imagery and graphics to this page as soon as I aquire them from my peers!</em></p>
+            </div>
+</div>
         <?php include "../parts/footer.php" ?>
 
     </main>
