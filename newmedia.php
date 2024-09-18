4fbb83cf2314a1a10384c14b3a5ff8fc56944c5e
jaydeninniss <jayden.inniss@gmail.com>
2024-09-17 18:12:08 -0700

    GAnylitics Everywhere

--- newmedia.php
+++ newmedia.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jayden Inniss - New Media</title>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QPYBLBF2QD"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-QPYBLBF2QD');
+    </script>
+
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
