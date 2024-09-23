11251103b271c5e0c8d060b9e7010470702cfc21
jaydeninniss <jayden.inniss@gmail.com>
2024-09-23 15:35:47 -0700

    Typo in db name

--- photo-project.php
+++ photo-project.php
@@ -74,7 +74,7 @@
         $db_host = 'localhost';
         $db_user = 'root';
         $db_password = 'root';
-        $db_db = 'innis_301_img-gallery';
+        $db_db = 'innis301_img-gallery';
 
         $imgs = @new mysqli(
             $db_host,
