08c058340fb254113fe43287282eb7ed09cb0dde
jaydeninniss <jayden.inniss@gmail.com>
2024-10-21 20:01:09 -0700

    Chnaged Commercial to Client Projects

--- photo.php
+++ photo.php
@@ -81,7 +81,7 @@
 
         <div id="commercial">
             <div class="header-banner">
-                <h2>Commercial</h2>
+                <h2>Client Projects</h2>
             </div>
 
             <div class="grid">
