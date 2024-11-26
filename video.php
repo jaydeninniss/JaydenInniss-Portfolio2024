1c84b24e5070c1a773fbad3e3dedc3dffa0dac41
jaydeninniss <jayden.inniss@gmail.com>
2024-11-26 14:48:29 -0800

    Swapped new media and video page names

--- video.php
+++ video.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jayden Inniss - Videography</title>
+    <title>Jayden Inniss - New Media</title>
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-QPYBLBF2QD"></script>
@@ -22,16 +22,16 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="../styles/style.css">
-    <link rel="stylesheet" href="../styles/video.css">
+    <link rel="stylesheet" href="../styles/newmedia.css">
 
     <!-- GSAP -->
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
 
     <!-- SEO -->
-    <meta property="og:title" content="Video - Jayden Inniss">
+    <meta property="og:title" content="New Media Projects - Jayden Inniss">
     <meta property="og:type" content="website">
-    <meta property="og:URL" content="https://www.jaydeninniss.com/video">
+    <meta property="og:URL" content="https://www.jaydeninniss.com/newmedia">
     <meta property="og:image" content="https://www.jaydeninniss.com/media/logo/jaydeninniss_logo.svg">
     <meta property="og:description" content="Jayden Inniss is a sports/adventure photographer and videographer proudly based in Whistler, BC.">
     <meta name="robots" content="all"> 
@@ -44,73 +44,106 @@
 </head>
 <body>
 
+    <?php include "./parts/header.php" ?>
+    <?php include "parts/hamburger.php" ?>
+
     <main>
 
-    <?php include "./parts/header.php" ?>
-    <?php include "./parts/hamburger.php" ?>
-
     <!-- HERO IMAGE -->
-    <div id="video-hero">
+    <div id="newmedia-hero">
+        <div id="newmedia-hero-bg"></div>
+        <div id="newmedia-hero-text"><p id='hero'>NEW MEDIA PROJECTS</p></div>
+        <div id="newmedia-hero-foreground"></div>
     </div>
     <!-- END HERO IMAGE -->
 
     <div class="content-container">
+        <div id="title-holder">
+            <h1>Featured Projects</h1>
+        </div>
 
-                <div class="header-banner">
-                    <h2>Video Projects</h2>
+        <div class="project-banner-container">
+            <div class="project" id="project1">
+                <div class="project-info" id="project-info1">
+                    <div class="left-side-info">
+                        <img src="/media/img/projects/stsfilmfestprojectcover.png" alt="Sea to Sky Film Festival" class="mobile-cover">
+                        <h2>SEA TO SKY STUDENT SHORT FILM FESTIVAL</h2>
+                        <h3>Community Fundraising Event</h3>
+                        <p class="project-type">Self-Directed</p>
+                        <p class="project-desc">Two peers and I created an event as a grad fundraiser for our high school. After receiving 9 submitted films, we held a viewing night at our school for an audience of over 300. <br><br>Role: Co-Founder</p>
+                        <button class="cta-button">
+                            <a class="cta" href="projects/stsfilmfest">
+                                Read more
+                                <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
+                            </a>
+                        </button>
+                    </div> 
                 </div>
+            </div>
 
-                <section class="video-holder">
-                    <h3>Indigenous Teachings at School District 48</h3>
-                    <iframe class="yt-video" src="https://www.youtube-nocookie.com/embed/S4su3ClgqJk?si=IILFCfgJULBgErk5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    <p>The goal was to create an informational video featuring School District 48 teachers and trustees participating in a day of learning the traditional ways of the Sk̲wx̲wú7mesh and Lil̓wat7úl peoples. <br>Tanina Williams taught staff how to make Plank Drums and play a traditional Sk̲wx̲wú7mesh and Lil̓wat7úl game during a professional development day in Pemberton BC.</p>
-                    <!-- <button class="cta-button">
-                        <a class="cta" href="#">
-                            Read More
-                            <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
-                        </a>
-                    </button> -->
-                </section>
-
-                <section class="video-holder broadcast-card">
-                    <a href="/projects/broadcast" class="broadcast-link">
-                    <h3>Broadcast Television Experience</h3>    
-                    <div class="broadcast-image"></div>
-                    </a>
-                </section>
-
-                <section class="video-holder">
-                    <h3>Tummy Time Video Series - Sea to Sky Community Services</h3>
-                    <iframe class="yt-video" src="https://www.youtube.com/embed/izV_ByRCgYA?si=o4UHkEoRDAIuEiMo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    <iframe class="yt-video" src="https://www.youtube.com/embed/j9KbEd3iXu4?si=BqCjDCyqSFojXLj0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    <p>In collaboration with SSCS, I produced two videos explaining newborn babies' growth and development from crawling to walking. These educational videos are currently being used by local physicians as a resource for new parents.</p>
-                    <!-- <button class="cta-button">
-                        <a class="cta" href="">
-                            Read More
-                            <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
-                        </a>
-                    </button> -->
-                </section>
-
-                <section class="video-holder">
-                    <h3>Crystal Lodge - Interview with Ian McIntosh</h3>
-                    <iframe class="yt-video" src="https://www.youtube.com/embed/WUKQpHO5CeU?si=KpOMjsHg8wotM9ra" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    <!-- <p>Description</p> -->
-                    <!-- <button class="cta-button">
-                        <a class="cta" href="">
-                            Read More
-                            <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
-                        </a>
-                    </button> -->
-                </section>
-
-                <div class="header-banner" id="caboose">
-                    <h3><a href="#video-hero">Back to Top</a></h3>
+            <div class="project" id="project2">
+                <div class="project-info" id="project-info2">
+                    <div class="left-side-info">
+                        <img src="/media/img/projects/alpinearmourcover.png" alt="Alpine Armour" class="mobile-cover">
+                        <h2>Alpine Armour</h2>
+                        <h3>Marketing Campaign Pitch</h3>
+                        <p class="project-type">New Media, BCIT</p>
+                        <p class="project-desc">In a group of four, we designed a full marketing campaign to promote an event for a fictional outdoor apparel company. The final deliverables were presented as a marketing pitch to peers.<br><br>Role: Project Manager</p>
+                        
+                        <button class="cta-button">
+                            <a class="cta" href="./projects/alpinearmour">
+                                Read more
+                                <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
+                            </a>
+                        </button>
+                    </div> 
                 </div>
+            </div>
 
+            <div class="project" id="project3">
+                <div class="project-info" id="project-info3">
+                    <div class="left-side-info">
+                        <img src="/media/img/projects/uiuxprojectcover.png" alt="UIUX Project" class="mobile-cover">
+                        <h2>UI/UX</h2>
+                        <h3>Interface Design + User Testing</h3>
+                        <p class="project-type">New Media, BCIT</p>
+                        <p class="project-desc">After designing an interface following UI/UX principles, I improved the interface by conducting user testing and interpreting the results into a final report.<br><br>Role: UI/UX Designer</p>
+                        <button class="cta-button">
+                            <a class="cta" href="./projects/uiux">
+                                Read more
+                                <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
+                            </a>
+                        </button>
+                    </div> 
+                </div>
+            </div>
+            
+            <div class="project-banner-container">
+            <div class="project" id="project4">
+                <div class="project-info" id="project-info4">
+                    <div class="left-side-info">
+                        <img src="/media/img/projects/bodyboost/bodyboost1.jpeg" alt="Body Boost Project" class="mobile-cover">
+                        <h2>Body Boost</h2>
+                        <h3>Project Managment</h3>
+                        <p class="project-type">New Media, BCIT</p>
+                        <p class="project-desc">In a group of four , we designed high-fidelity mockups for a mobile application to gain experience with linear (waterfall) projects.<br><br>As the project manager , I planned out what tasks needed to be completed when and by who, based on each designer's strengths/preferences.<br><br>Role: Project Manager</p>
+                        <button class="cta-button">
+                            <a class="cta" href="projects/bodyboost">
+                                Read more
+                                <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
+                            </a>
+                        </button>
+                    </div> 
+                </div>
+            </div>
+        </div>
         <?php include "./parts/footer.php" ?>
     </div>
+
     </main>
+
+    <script src="./js/parallaxhero/parallaxhero_newmedia.js"></script>
+    <script src="./js/newmedia.js"></script>
 
 </body>
 </html>
---
+++ videoold.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jayden Inniss - Videography</title>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QPYBLBF2QD"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-QPYBLBF2QD');
+    </script>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anybody:ital,wght@0,100..900;1,100..900&family=Be+Vietnam+Pro:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/video.css">
+
+    <!-- GSAP -->
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+
+    <!-- SEO -->
+    <meta property="og:title" content="Video - Jayden Inniss">
+    <meta property="og:type" content="website">
+    <meta property="og:URL" content="https://www.jaydeninniss.com/video">
+    <meta property="og:image" content="https://www.jaydeninniss.com/media/logo/jaydeninniss_logo.svg">
+    <meta property="og:description" content="Jayden Inniss is a sports/adventure photographer and videographer proudly based in Whistler, BC.">
+    <meta name="robots" content="all"> 
+    <meta name="author" content="Jayden Inniss">
+    <meta name="description" content="Jayden Inniss is a sports/adventure photographer and videographer proudly based in Whistler, BC.">
+    <meta name="keywords" content="Photography, Videography, Jayden Inniss, Whistler, Camera, Photography Services in Whistler, Filmer in Whistler, Biking, Skiing, Hiking, Camping, Whistler British Columbia Photo/Video, Sport Photographer, Landscape Photography, Action Sport Photography and Videography">
+    <link rel="icon" type="image/x-icon" href="./media/logo/jaydeninniss_logo.svg">
+    <!-- End of SEO -->
+
+</head>
+<body>
+
+    <main>
+
+    <?php include "./parts/header.php" ?>
+    <?php include "./parts/hamburger.php" ?>
+
+    <!-- HERO IMAGE -->
+    <div id="video-hero">
+    </div>
+    <!-- END HERO IMAGE -->
+
+    <div class="content-container">
+
+                <div class="header-banner">
+                    <h2>Video Projects</h2>
+                </div>
+
+                <section class="video-holder">
+                    <h3>Indigenous Teachings at School District 48</h3>
+                    <iframe class="yt-video" src="https://www.youtube-nocookie.com/embed/S4su3ClgqJk?si=IILFCfgJULBgErk5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <p>The goal was to create an informational video featuring School District 48 teachers and trustees participating in a day of learning the traditional ways of the Sk̲wx̲wú7mesh and Lil̓wat7úl peoples. <br>Tanina Williams taught staff how to make Plank Drums and play a traditional Sk̲wx̲wú7mesh and Lil̓wat7úl game during a professional development day in Pemberton BC.</p>
+                    <!-- <button class="cta-button">
+                        <a class="cta" href="#">
+                            Read More
+                            <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
+                        </a>
+                    </button> -->
+                </section>
+
+                <section class="video-holder broadcast-card">
+                    <a href="/projects/broadcast" class="broadcast-link">
+                    <h3>Broadcast Television Experience</h3>    
+                    <div class="broadcast-image"></div>
+                    </a>
+                </section>
+
+                <section class="video-holder">
+                    <h3>Tummy Time Video Series - Sea to Sky Community Services</h3>
+                    <iframe class="yt-video" src="https://www.youtube.com/embed/izV_ByRCgYA?si=o4UHkEoRDAIuEiMo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <iframe class="yt-video" src="https://www.youtube.com/embed/j9KbEd3iXu4?si=BqCjDCyqSFojXLj0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <p>In collaboration with SSCS, I produced two videos explaining newborn babies' growth and development from crawling to walking. These educational videos are currently being used by local physicians as a resource for new parents.</p>
+                    <!-- <button class="cta-button">
+                        <a class="cta" href="">
+                            Read More
+                            <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
+                        </a>
+                    </button> -->
+                </section>
+
+                <section class="video-holder">
+                    <h3>Crystal Lodge - Interview with Ian McIntosh</h3>
+                    <iframe class="yt-video" src="https://www.youtube.com/embed/WUKQpHO5CeU?si=KpOMjsHg8wotM9ra" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <!-- <p>Description</p> -->
+                    <!-- <button class="cta-button">
+                        <a class="cta" href="">
+                            Read More
+                            <img src="../media/icons/button-arrow.svg" alt="Right Arrow">
+                        </a>
+                    </button> -->
+                </section>
+
+                <div class="header-banner" id="caboose">
+                    <h3><a href="#video-hero">Back to Top</a></h3>
+                </div>
+
+        <?php include "./parts/footer.php" ?>
+    </div>
+    </main>
+
+</body>
+</html>
