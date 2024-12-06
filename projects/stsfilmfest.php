93ac1818dc8d9181ae9c99edca966594fad6a63b
jaydeninniss <jayden.inniss@gmail.com>
2024-12-05 21:34:31 -0800

    CSS looks pretty good, contents going in

--- projects/stsfilmfest.php
+++ projects/stsfilmfest.php
@@ -62,25 +62,64 @@
                         <p>Whistler Secondary Grad Class 2023</p>
                 </div>
                 <h3 class="question" id="first">Overview</h3>
-                <p class="answer">My two peers and I created a film festival designed for youth in The Sea to Sky Corridor to showcase their filmmaking talent. </p><p class="highlight">Sho Brooks, Kenta Tanaka and I led this initiative. </p><p> The competition was designed as a fundraiser for our graduating class at Whistler Secondary School. After receiving 9 submitted films, we held a viewing night at our school for an audience of over 300. This event was a huge success: We exceeded our projected profit by $4,000, doubled our expected audience and gave a platform for young aspiring filmmakers to display their work in front of the community. </p>
+                <p class="answer">My two peers and I created a film festival designed for youth in The Sea to Sky Corridor to showcase their filmmaking talent. Sho Brooks, Kenta Tanaka and I led this initiative. </p><p class="answer"> The competition was designed as a fundraiser for our graduating class at Whistler Secondary School. After receiving <span class="highlight">9 submitted films,</span> we held a viewing night at our school for an <span class="highlight">audience of over 300.</span> This event was a huge success: We <span class="highlight">exceeded our projected profit by $4,000,</span> doubled our expected audience and gave a platform for young aspiring filmmakers to display their work in front of the community. </p>
+
+                <img src="COVER" alt="">
                 
                 <h3 class="question">Competition Details</h3>
-                <p class="answer">To celebrate what makes the Sea to Sky region unique, the Whistler Secondary Short Film Festival debuts as a student filmmaking competition produced by the graduating class of 2023 where you could win great prizes! The competition is open to anyone in grades 8 to 12 residing in Squamish, Whistler, Pemberton and Mt. Currie, and attending an SD48 public or independent school with a phone, GoPro, or camera with a story to tell. </p>
+                <p class="answer">To celebrate what makes the Sea to Sky region unique, the Whistler Secondary Short Film Festival debuts as a student filmmaking competition produced by the graduating class of 2023 where you <span class="highlight">could win great prizes!</span></p><p class="answer"><span class="highlight">The competition is open to anyone in grades 8 to 12</span> residing in Squamish, Whistler, Pemberton and Mt. Currie, and attending an SD48 public or independent school with a phone, GoPro, or camera with a story to tell. </p>
+
+                <div class="image-container">
+                    <img src="DETAILS " alt="">
+                </div>
 
                 <h3 class="question">Advertising</h3>
                 <p class="answer">Creativity is your only limit. Student filmmakers are tasked to shoot, edit and produce a 3 to 5 minute short film within the Sea to Sky Corridor - taking their creative vision from concept to reality. The top ten films will premiere on the big screen for an audience of over 200 at Whistler Secondary. Over $2,000 in prizes will be awarded to the top three films plus a People's Choice Award chosen by the audience. </p>
 
+                <div class="image-container">
+                    <img src="../media/img/projects/broadcast/lugecam.jpeg" alt="">
+                    <img src="../media/img/projects/broadcast/lugeselfie2.jpeg" alt="">
+                    <img src="../media/img/projects/broadcast/lugeselfie2.jpeg" alt="">
+                </div>
+
                 <p class="answer"><em>Calling Sea to Sky Corridor high school students! The Sea to Sky region is known for inspiring adventurers and storytellers. Whistler Secondary wants to see how it inspires you in its inaugural Short Film Festival. Submit a short film for your chance to win great prizes!</em></p>
 
-                <h3 class="question">Judging</h3>
-                <p class="answer">The judges, composed of professional filmmakers, will select up to 20 films, three to five minute films to be part of the Whistler Secondary Short Film Festival on April 20, 2023 at Whistler Secondary.<br><br>Entries will be judged based on their application's overall concept, creativity, and originality.<br><br>There are no restrictions on your creativity or how you choose to capture the magic that makes the Sea to Sky region so unique. And there are no limitations on how the film is shot or how you interpret the theme. You can shoot live-action, animation, stop-motion, documentary, use actors, cutouts, puppets, or animals. The choice is yours!</p>
+                <h3 class="question">Judging Panel</h3>
+                <p class="answer">The judging panel was composed of a hand-picked group of <span class="highlight">proffessional filmmakers </span>from the Sea to Sky corridor.<br><br>Films will be judged based on their application's overall concept, creativity, and originality.</p>
+
+                <div class="image-container">
+                    <img src="../media/img/projects/broadcast/lugecam.jpeg" alt="">
+                </div>
+                
+                <p class="answer">There are no restrictions on your creativity or how you choose to capture the magic that makes the Sea to Sky region so unique.</p>
+
+                <div class="image-container">
+                    <img src="../media/img/projects/broadcast/lugecam.jpeg" alt="">
+                    <img src="../media/img/projects/broadcast/lugeselfie2.jpeg" alt="">
+                </div>
 
                 <h3 class="question">Film Screening Night</h3>
-                <p class="answer">After receiving 9 submitted films, we held a viewing night at our school for an audience of over 300. This event was a huge success: We exceeded our projected profit by $4,000, doubled our expected audience and gave a platform for young aspiring filmmakers to display their work in front of the community. <br><br>Myself and Kenta Tanaka hosted the night as MCs. </p>
+                <p class="answer">After receiving 9 submitted films, we held a viewing night at our school for an audience of over 300. </p>
+
+                <div class="image-container">
+                    <img src="../media/img/projects/stsfilmfestprojectcover.png" alt="">
+                </div>
+                
+                <p class="answer">This event was a huge success: We exceeded our projected profit by $4,000, doubled our expected audience and gave a platform for young aspiring filmmakers to display their work in front of the community. <br><br>Myself and Kenta Tanaka hosted the night as MCs. </p>
+
+                <div class="image-container">
+                    <img src="../media/img/projects/broadcast/lugecam.jpeg" alt="">
+                    <img src="../media/img/projects/broadcast/lugeselfie2.jpeg" alt="">
+                </div>
 
                 <h3 class="question">Conclusion</h3>
-                <p class="answer">This event was a highlight of high school. Providing an opportunity for youth filmmakers in Whistler was a way to give back to the community for everything it has provided to me. In 2017, the World Ski and Snowboard Festival's Olympus 72-Hour Filmmaker Showdown chose my film as one of the top 10 films. It was screened in front of 1,600, which was mind-blowing when I was in grade 7. This event was full circle as I was able to give filmmakers a similar opportunity to present their films and gain exposure within the community.<br><br>This whole event was so rewarding and I enjoyed every second of planning, developing and finally MCing the event alongside my incredibly talented peers: Sho Brooks and Kenta Tanaka.</p>
-                <p class="answer"><em>I will absolutely add imagery and graphics to this page as soon as I aquire them from my peers!</em></p>
+                <p class="answer"><span class="highlight"> This event was a highlight of our high school experience. </span></p>
+                <p class="answer">
+                Providing an opportunity for youth filmmakers in Whistler was <span class="highlight">a way to give back to the community for everything it has provided to me. </span></p><p class="answer">In 2017, the World Ski and Snowboard Festival's Olympus 72-Hour Filmmaker Showdown chose my film as one of the top 10 films. It was screened in front of 1,600, which was mind-blowing when I was in grade 7. <span class="highlight">This event was full circle for me </span>as I was able to give filmmakers a similar opportunity to present their films and gain exposure within the community.<br><br>This whole event was so rewarding and I enjoyed every second of planning, developing and finally MCing the event alongside my incredibly talented peers: Sho Brooks and Kenta Tanaka.</p>
+
+                <div class="image-container">
+                    <img src="../media/img/projects/broadcast/lugecam.jpeg" alt="">
+                </div>
             </div>
         </div>
         <?php include "../parts/footer.php" ?>
