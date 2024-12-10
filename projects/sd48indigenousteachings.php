db2c5cd0430cdf5857b6a25c39a572680504dd3c
jaydeninniss <jayden.inniss@gmail.com>
2024-12-09 23:54:36 -0800

    Indigenous Teachings Page Rough Done

--- projects/sd48indigenousteachings.php
+++ projects/sd48indigenousteachings.php
@@ -49,26 +49,57 @@
     <?php include "../parts/hamburger.php" ?>
 
     <main>
-        <!-- Header Img -->
-        <div class="hero">
-            <img src="../media/img/clients/ubcsnb/jaydeninniss_ubcsnb_cover.webp" alt="UBC Ski and Board Logo">
+    <!-- Header Img -->
+    <div class="hero">
+        <img src="../media/img/banners/tanina-banner.webp" alt="">
+    </div>
+    <!-- END Header Img -->
+
+    <div class="content-container">
+
+
+    <section class="video-holder">
+        <h2>SD48 Day of Indigenous Teachings</h2>
+        <p>School District 48 - Paid Video Project</p>
+        <iframe class="yt-video" src="https://www.youtube-nocookie.com/embed/S4su3ClgqJk?si=IILFCfgJULBgErk5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>The goal was to create an <span class = "highlight">educational video</span> that would help to raise awareness about the traditional ways of the Sk̲wx̲ẃ7mesh and Lil̓wat7́l peoples. The <span class = "highlight">10-minute video</span> featured School District 48 teachers and trustees participating in a day of learning during a proffesional development day.<br><br><span class = "highlight">Tanina Williams taught staff how to make Plank Drums</span> and play a traditional Sk̲wx̲wú7mesh and Lil̓wat7úl game at Signal Hill Elemntary School in Pemberton, BC.</p>
+    </section>
+
+        <div class="qa-container">
+
+            <h3 class="question" id="goal">Goal</h3>
+            <p class="answer">
+                The goal was to create an informational video featuring School District 48 teachers and trustees participating in a day of learning the traditional ways of the Sk̲wx̲wú7mesh and Lil̓wat7úl peoples. Tanina Williams taught staff how to make Plank Drums and play a traditional Sk̲wx̲wú7mesh and Lil̓wat7úl game during a professional development day in Pemberton BC.
+            </p>
+
+            <h3 class="question" id="pre-production">Pre-Production</h3>
+            <p class="answer">
+                Tanina Williams and I began this project by determining what would be included in the video through a shot list and reviewing existing Sk̲wx̲wú7mesh and Lil̓wat7úl videos. We decided to focus on the people involved in the teachings, rather than the teachings themselves, as the video was intended to connect with other School District staff and students by featuring familiar faces.
+            </p>
+            <p class="answer">
+                We finalized pre-production with several video calls to confirm the requirements and the desired look and feel of the video. I wrote a comprehensive shot list to streamline production.
+            </p>
+
+            <h3 class="question" id="production">Production</h3>
+            <p class="answer">
+                Filming began early in Pemberton, BC, where I captured the excitement of staff gathering for the professional development day. Documenting the creation of plank drums led by Norman Wallace was especially rewarding. Norman, acting as our fire manager, guided staff through burning coals and sandwiching them between wood planks.
+            </p>
+            <p class="answer">
+                The day's atmosphere was filled with respect and curiosity, which shines through in the footage. We wrapped up with interviews featuring School District 48's trustees, who reflected on the importance of passing down Indigenous teachings within the school system.
+            </p>
+
+            <h3 class="question" id="post-production">Post-Production</h3>
+            <p class="answer">
+                The editing process was smooth. Tanina invited me to record members of the Lil̓wat7úl nation performing, providing background music for the video. I also gathered images to illustrate the steps and story for Lahal (the bone game) and the plank drum process, combining them with motion graphics and text to complete the narrative.
+            </p>
+            <p class="answer">
+                Four months later, Tanina was a guest speaker at The Sea to Sky Student Short Film Festival, which I co-founded. She was so thrilled with the video that she requested to play it for an audience of over 350 people during the festival's screening night.
+            </p>
         </div>
-        <!-- END Header Img -->
+    </div>
+    <?php include "../parts/footer.php" ?>
+</main>
 
-        <div class="content-container">
-            <div class="qa-container">
-                <div class="project-description">
-                        <h2>SD48 Day of Indigenous Teachings</h2>
-                        <p>School District 48 - Paid Video Project</p>
-                </div>
-
-                <h3 class="question" id="first">2024 Beach Kegger</h3>
-
-            </div>
-        </div>
-        <?php include "../parts/footer.php" ?>
-
-    </main>
 
 </body>
 </html>
